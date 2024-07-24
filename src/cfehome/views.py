cdcb65894a1591f4c638c914b1b34e391a9caeb0
Richlue King <richlueking302@gmail.com>
2024-07-23 22:25:54 -0700

    Hello world msg third

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
 from visits.models import PageVisit
 
-def home_page_view(request, *args, **kwargs):
-    qs=PageVisit.objects.all()
-    page_qs=PageVisit.objects.filter(path=request.path)
-        
-    my_title="My Page"
-    my_context={
+
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
+    html_template = "home.html"
+    my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "total_visit": qs.count(),
-        
-        "percent": (page_qs.count() * 100.0) / qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
     }
-    
-    html_template="home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
+
+
+
