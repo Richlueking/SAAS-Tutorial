f09a70643d76287e776de557e8a82742c2f9f12f
Richlue King <richlueking302@gmail.com>
2024-07-23 22:36:40 -0700

    section-1

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+
 from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_view),
+    path("", home_view), #index page -> root page
     path("about/", about_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
-
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
